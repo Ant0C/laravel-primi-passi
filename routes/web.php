8794f6e398c4ea8a6b20514f3fc8a8feeb6ad91d
Ant0C <cafeo97@gmail.com>
2023-04-04 16:42:40 +0200

    Create differents routs Bonus

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+Route::get('/services', function () {
+    return view('services');
 });
